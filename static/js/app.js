558f83f3af854f9b8ebb700d3a1f5fb5acbed793
lucasquinlan <128264735+lucasquinlan@users.noreply.github.com>
2023-10-19 20:33:21 -0500

    leaflet map loading correctly

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,9 @@
+// Initialize the map
+let myMap = L.map("map").setView([37.09, -95.71], 4);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
 
 d3.json("/hidden/states").then((states) => {
     console.log(states);
@@ -41,14 +47,12 @@ function generateStateLeaflet(state) {
 
         let city_data = data.cities;
 
-        let myMap = L.map("map", {
-            center: [data.state_lat, data.state_lng],
-            zoom: 5
+        // Clear the map before adding new data
+        myMap.eachLayer(function (layer) {
+            if (layer instanceof L.Circle) {
+                myMap.removeLayer(layer);
+            }
         });
-
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(myMap);
 
         for (let i = 0; i < city_data.length; i++) {
             cur_city = city_data[i];
@@ -62,9 +66,70 @@ function generateStateLeaflet(state) {
                 .addTo(myMap);
             }
         }
-
     });
 }
 
 generateStateSightsBarGraph("Alabama");
 generateStateLeaflet("Alabama");
+
+
+
+
+
+// Initialize the map
+//function generateLeaflet() {
+//    let map = L.map('map').setView([39.82, -98.58], 5);
+//    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//    maxZoom: 19,
+//    }).addTo(map);
+
+    // Load state boundary data as a GeoJSON layer. State boundary geojson data from 
+    //https://public.opendatasoft.com/explore/dataset/us-state-boundaries/table/?flg=en-us
+//    d3.json('../static/data/us-state-boundaries.geojson').then(stateBoundaryData => {
+        // Create the GeoJSON layer using the loaded data
+//        let stateBoundaryLayer = L.geoJSON(stateBoundaryData, {
+//            style: {
+//                color: 'black',
+//                weight: 0.5,
+//                fill: false,
+//            }
+//        }).addTo(map);
+//    });
+
+
+    // Create an empty object to store state clusters
+//    let markers = L.markerClusterGroup();
+
+    // Make request to leaflet API endpoint
+//    d3.json("/leaflet").then(data => {
+
+            // Add each marker to the MarkerClusterGroup instead of directly to the map
+//            data.forEach(item => {
+//                let lat = item.latitude;
+//                let lng = item.longitude;
+//                let state = item.state;
+//                let name = item.city + ", " + state;
+
+//                if (lat !== undefined && lng !== undefined) {
+//                    let marker = L.marker([lat, lng])
+//                        .bindPopup(name + '<br>' + item.location);
+
+                    // Create a cluster for the state if it doesn't exist
+//                    if (lat !== undefined && lng !== undefined) {
+//                    let marker = L.marker([lat, lng])
+//                        .bindPopup(name + '<br>' + item.location);
+
+                    // Add the marker to the single MarkerClusterGroup
+//                    markers.addLayer(marker);
+//                } else {
+//                    console.warn(`Skipping item (${name}) without valid coordinates.`);
+//                }
+//                }});    
+    
+
+            // Add the single MarkerClusterGroup to the map
+//            map.addLayer(markers);
+//        });
+//}
+
+//generateLeaflet()
