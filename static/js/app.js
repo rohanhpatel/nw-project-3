6f550fb044d71cd6651de37c4440c5ec9e52f828
lucasquinlan <128264735+lucasquinlan@users.noreply.github.com>
2023-10-22 09:14:00 -0500

    Adding leaflet state boundaries and zoom feature

--- static/js/app.js
+++ static/js/app.js
@@ -5,6 +5,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
+// Load state boundary data as a GeoJSON layer. State boundary geojson data from 
+//https://public.opendatasoft.com/explore/dataset/us-state-boundaries/table/?flg=en-us
+d3.json('../static/data/us-state-boundaries.geojson').then(stateBoundaryData => {
+    // Create the GeoJSON layer using the loaded data
+    let stateBoundaryLayer = L.geoJSON(stateBoundaryData, {
+        style: {
+            color: 'black',
+            weight: 0.5,
+            fill: false,
+        }
+    }).addTo(myMap);
+});
+
 d3.json("/hidden/states").then((states) => {
     console.log(states);
 
@@ -54,20 +67,41 @@ function generateStateLeaflet(state) {
             }
         });
 
-        for (let i = 0; i < city_data.length; i++) {
-            cur_city = city_data[i];
-            if (cur_city["lat"] != null && cur_city["lng"] != null) {
-                L.circle([cur_city["lat"], cur_city["lng"]], {
-                    fillOpacity: 0.75,
-                    color: "black",
-                    fillColor: "white",
-                    radius: cur_city["count"] * 100,
-                }).bindPopup(`<p>City: ${cur_city["name"]}</p><p>Number of Sightings: ${cur_city["count"]}</p>`)
-                .addTo(myMap);
-            }
-        }
-    });
-}
+        // Load the state center coordinates and zoom level from your JSON data
+        d3.json("../static/data/state_centers.json").then((centerData) => {
+            // Find the state data by matching the state name
+            const stateInfo = centerData.find((item) => item.name === state);
 
-generateStateLocationBarGraph("Alabama");
-generateStateLeaflet("Alabama");
+            if (stateInfo) {
+                const stateCenterLat = stateInfo.lat;
+                const stateCenterLng = stateInfo.lon;
+                const stateZoomLevel = stateInfo.zoom*1.2;
+
+                console.log("Center Coordinates:", stateCenterLat, stateCenterLng);
+                console.log("Zoom Level:", stateZoomLevel);
+
+                const stateCenter = [stateCenterLat, stateCenterLng];
+
+                // Set the view to center on the state with an appropriate zoom level
+                myMap.setView(stateCenter, stateZoomLevel);
+            }
+
+            for (let i = 0; i < city_data.length; i++) {
+                cur_city = city_data[i];
+                if (cur_city["lat"] != null && cur_city["lng"] != null) {
+                    L.circle([cur_city["lat"], cur_city["lng"]], {
+                        fillOpacity: 0.75,
+                        color: "black",
+                        fillColor: "white",
+                        radius: cur_city["count"] * 100,
+                    }).bindPopup(`<p>City: ${cur_city["name"]}</p><p>Number of Sightings: ${cur_city["count"]}</p>`)
+                    .addTo(myMap);
+                }
+            }
+        });
+    });
+}    
+
+
+generateStateLocationBarGraph("");
+generateStateLeaflet("");
